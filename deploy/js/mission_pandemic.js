6ab46a559c014d1c274935f2cbe5efe864789967
Adam Bengis <itbengis@gmail.com>
2020-11-19 13:17:04 -0500

    merge npc and hand sanitizer infection effects

--- deploy/js/mission_pandemic.js
+++ deploy/js/mission_pandemic.js
@@ -82,7 +82,7 @@ class KormanQuad extends Phaser.Scene {
     collideCb(objA, objB) {
         if((objA.name === "player" && objB.name === "end")
             || (objB.name === "player" && objA.name === "end")) {
-            this.add.text(400, 300, "You Win!", { color: "green" });
+            this.add.text(400, 300, "You Win!", { fontSize: "24px", color: "green" });
             this.gameOver = true;
             this.player.setVelocityX(0).setVelocityY(0);
         }
@@ -90,7 +90,6 @@ class KormanQuad extends Phaser.Scene {
 
     create(data)  {
         this.add.image(400, 300, "background").setDisplaySize(800, 600);
-
         const borders = this.physics.add.staticGroup();
         // start zone
         borders.create(162, 170, null).setVisible(false).setSize(10, 10).setName("start");
@@ -113,10 +112,14 @@ class KormanQuad extends Phaser.Scene {
         npcs.create(302, 144, 'npc1').setScale(0.25).setAngle(180);
         npcs.create(295, 342, 'npc2').setScale(0.25).setAngle(90);
         npcs.create(345, 376, 'npc3').setScale(0.25).setAngle(0);
+        this.physics.add.overlap(this.player, npcs, () => this.touchNPC());
 
-        this.physics.add.sprite(470, 520, 'handsan').setScale(0.25);
+        this.handsan = this.physics.add.sprite(470, 520, 'handsan').setScale(0.25);
         this.physics.add.overlap(this.player, this.handsan, this.collectHandSanitizer, null, this);
-        
+
+        const infectionTextStyle = {fontSize: "18px", color: "black"};
+        this.add.text(260, 570, "Infection Potential:", infectionTextStyle);
+        this.infectionText = this.add.text(482, 570, "0%", infectionTextStyle);
         this.cursors = this.input.keyboard.createCursorKeys();
     }
 
@@ -129,11 +132,13 @@ class KormanQuad extends Phaser.Scene {
 
       if(incr > 0) {
         if(new_inf_level > 100) new_inf_level = 100;
+
         for(let i = inf_level; i < new_inf_level; i++) {
           this.graphics.fillCircle(400+115*(i/55-1), 549, 14);
         }
       } else {
         if(new_inf_level < 0) new_inf_level = 0;
+
         this.graphics.clear();
         for(let i = 0; i < new_inf_level; i++) {
           this.graphics.fillCircle(400+115*(i/55-1), 549, 14);
@@ -166,25 +171,39 @@ class KormanQuad extends Phaser.Scene {
       else {
           this.player.setVelocityY(0);
       }
-      
+      // endgame check
+      if(this.player.getData("infection_level") >= 100)
+      {
+        this.gameOver = true;
+        this.loseText = this.add.text(400, 300, "You Lost!", { fontSize: "24px", color: "red" });
+        this.player.setVelocityX(0).setVelocityY(0);
+      }
     }
 
     updateOutOfGame(time, delta) {
-
+      
     }
 
     update(time, delta) {
-        if(!this.gameOver) {
-            this.updateGameplay(time, delta);
-        }
-        else {
-          this.updateOutOfGame(time, delta);
-        }
+      if(!this.gameOver) {
+          this.updateGameplay(time, delta);
+      }
+      else {
+        this.updateOutOfGame(time, delta);
+      }
+
+      this.infectionText.setText(`${this.player.getData("infection_level")}%`);
     }
+
+    touchNPC(player, npc)
+    {
+        this.incrPlayerInfectionLevel(1);
+    }
+
     collectHandSanitizer (player, handsan)
     {
-        this.handsan.disableBody(true, true);
-
+        this.handsan.destroy();
+        this.incrPlayerInfectionLevel(-100);
     }
 }
 // All files in the src/game directory are concatenated on save
