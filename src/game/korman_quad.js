1da687c398f5e9581568e6265ed178338d111ff4
Adam Bengis <itbengis@gmail.com>
2020-11-19 10:24:26 -0500

    NPC placement in Korman Quad

--- src/game/korman_quad.js
+++ src/game/korman_quad.js
@@ -5,8 +5,10 @@ class KormanQuad extends Phaser.Scene {
 
     preload() {
         this.load.image("background", "../assets/backgrounds/korman_quad.jpg");
-        this.load.image("player", "../assets/Player2.svg");
-        this.load.image("npc", "../assets/npc_top.png");
+        this.load.image("player", "../assets/characters/red_man_top.svg");
+        this.load.image("npc1", "../assets/characters/yellow_woman_top.svg");
+        this.load.image("npc2", "../assets/characters/blue_man_top.svg");
+        this.load.image("npc3", "../assets/characters/red_woman_top.svg");
     }
 
     init(data) {
@@ -31,11 +33,14 @@ class KormanQuad extends Phaser.Scene {
         // top right yellow
         borders.create(632, 80, null).setVisible(false).setSize(185, 145).setName("end");
 
-        this.player = this.physics.add.sprite(225, 350, 'player').setScale(0.25).setName("player");
+        this.player = this.physics.add.sprite(161, 171, 'player').setScale(0.25).setAngle(180).setName("player");
         this.player.setCollideWorldBounds(true);
-
         this.physics.add.collider(this.player, borders, (objA, objB) => this.collideCb(objA, objB));
 
+
+        this.physics.add.sprite(302, 144, 'npc1').setScale(0.25).setAngle(180);
+        this.physics.add.sprite(295, 342, 'npc2').setScale(0.25).setAngle(90);
+        this.physics.add.sprite(345, 376, 'npc3').setScale(0.25).setAngle(0);
     }
 
     updateGameplay(time, delta) {
@@ -43,22 +48,22 @@ class KormanQuad extends Phaser.Scene {
 
       if (cursors.left.isDown) {
           this.player.setVelocityX(-160);
-          this.player.angle = -90;
+          this.player.setAngle(-90);
       }
       else if (cursors.right.isDown) {
           this.player.setVelocityX(160);
-          this.player.angle = 90;
+          this.player.setAngle(90);
       }
       else {
           this.player.setVelocityX(0);
       }
       if (cursors.up.isDown) {
           this.player.setVelocityY(-160);
-          this.player.angle = 0;
+          this.player.setAngle(0);
       }
       else if (cursors.down.isDown) {
           this.player.setVelocityY(160);
-          this.player.angle = 180;
+          this.player.setAngle(180);
       }
       else {
           this.player.setVelocityY(0);
