614b3c69c4280daf68b02346d6b209663a608eaa
Adam Bengis <itbengis@gmail.com>
2020-11-19 12:56:30 -0500

    added start and end zones and added hand sanitizer sprite

--- deploy/js/mission_pandemic.js
+++ deploy/js/mission_pandemic.js
@@ -72,6 +72,7 @@ class KormanQuad extends Phaser.Scene {
         this.load.image("npc2", "../assets/characters/blue_man_top.svg");
         this.load.image("npc3", "../assets/characters/red_woman_top.svg");
         this.load.image("inf_bar", "../assets/icons/infection_bar.svg");
+        this.load.image("handsan", "../assets/icons/hand_sanitizer.svg")
     }
 
     init(data) {
@@ -81,7 +82,7 @@ class KormanQuad extends Phaser.Scene {
     collideCb(objA, objB) {
         if((objA.name === "player" && objB.name === "end")
             || (objB.name === "player" && objA.name === "end")) {
-            this.add.text(400, 300, "Success!", { color: "green" });
+            this.add.text(400, 300, "You Win!", { color: "green" });
             this.gameOver = true;
             this.player.setVelocityX(0).setVelocityY(0);
         }
@@ -90,6 +91,11 @@ class KormanQuad extends Phaser.Scene {
     create(data)  {
         this.add.image(400, 300, "background").setDisplaySize(800, 600);
 
+        const borders = this.physics.add.staticGroup();
+        // start zone
+        borders.create(162, 170, null).setVisible(false).setSize(10, 10).setName("start");
+        // end zone
+        borders.create(290, 420, null).setVisible(false).setSize(10, 10).setName("end");
 
         this.player = this.physics.add.sprite(161, 171, 'player')
           .setScale(0.25)
@@ -101,13 +107,15 @@ class KormanQuad extends Phaser.Scene {
         this.add.image(400, 550, "inf_bar").setScale(2);
         this.graphics = this.add.graphics();
 
-        const borders = this.physics.add.staticGroup();
         this.physics.add.collider(this.player, borders, (objA, objB) => this.collideCb(objA, objB));
 
         const npcs = this.physics.add.staticGroup();
         npcs.create(302, 144, 'npc1').setScale(0.25).setAngle(180);
         npcs.create(295, 342, 'npc2').setScale(0.25).setAngle(90);
         npcs.create(345, 376, 'npc3').setScale(0.25).setAngle(0);
+
+        this.physics.add.sprite(470, 520, 'handsan').setScale(0.25);
+        this.physics.add.overlap(this.player, this.handsan, this.collectHandSanitizer, null, this);
         
         this.cursors = this.input.keyboard.createCursorKeys();
     }
@@ -131,9 +139,7 @@ class KormanQuad extends Phaser.Scene {
           this.graphics.fillCircle(400+115*(i/55-1), 549, 14);
         }
       }
-      this.player.setData({
-        "infection_level": new_inf_level,
-      });
+      this.player.setData("infection_level", new_inf_level); 
     }
 
     updateGameplay(time, delta) {
@@ -160,6 +166,7 @@ class KormanQuad extends Phaser.Scene {
       else {
           this.player.setVelocityY(0);
       }
+      
     }
 
     updateOutOfGame(time, delta) {
@@ -173,6 +180,11 @@ class KormanQuad extends Phaser.Scene {
         else {
           this.updateOutOfGame(time, delta);
         }
+    }
+    collectHandSanitizer (player, handsan)
+    {
+        this.handsan.disableBody(true, true);
+
     }
 }
 // All files in the src/game directory are concatenated on save
