1a552708a574dcee14ffae21730b20eadec1d025
Adam Bengis <itbengis@gmail.com>
2020-12-07 18:26:50 -0500

    Add movement to Lancaster NPCs

--- deploy/js/mission_pandemic.js
+++ deploy/js/mission_pandemic.js
@@ -359,17 +359,103 @@ class LancasterWalk extends Phaser.Scene {
       this.physics.add.collider(this.player, barriers);
       this.physics.add.collider(this.player, zones, (player, zone) => this.collideZone(player, zone));
 
-      const npcs = this.physics.add.staticGroup();
-      npcs.create(620, 160, 'npc1').setScale(0.25).setAngle(180).setCircle(70)
+      const npcs = this.physics.add.group();
+      const npc1 = npcs.create(620, 160, 'npc1')
+        .setScale(0.25)
+        .setAngle(170)
+        .setOffset(-75, -75)
+        .setCircle(150)
         .setData("mask", false);
-      npcs.create(290, 305, 'npc1').setScale(0.25).setAngle(-90).setCircle(60)
+      this.tweens.add({
+        targets: npc1,
+        props: {
+          y: { from: npc1.y, to: npc1.y+80 },
+          x: { from: npc1.x, to: npc1.x+30 },
+          fake: {from: 0, to: 0}
+        },
+        flipY: true,
+        flipX: true,
+        ease: 'Linear',      
+        duration: 1800 - this.level * 400,
+        repeat: -1,       
+        yoyo: true
+      });
+      const npc2 = npcs.create(270, 305, 'npc1')
+        .setScale(0.25)
+        .setAngle(90)
+        .setOffset(-75, -75)
+        .setCircle(150)
         .setData("mask", false);
-      npcs.create(345, 265, 'npc3').setScale(0.25).setAngle(0).setCircle(60)
+      this.tweens.add({
+        targets: npc2,
+        props: {
+          x: { from: npc2.x, to: npc2.x+100 },
+        },
+        flipY: true,
+        flipX: true,
+        ease: 'Linear',      
+        duration: 1800 - this.level * 400,
+        repeat: -1,       
+        yoyo: true
+      });
+      const npc3 = npcs.create(370, 260, 'npc3')
+        .setScale(0.25)
+        .setAngle(-50)
+        .setOffset(-75, -75)
+        .setCircle(150)
         .setData("mask", true);
-      npcs.create(550, 150, 'npc2').setScale(0.25).setAngle(-90).setCircle(60)
+      this.tweens.add({
+        targets: npc3,
+        props: {
+          y: { from: npc3.y, to: npc3.y-40 },
+          x: { from: npc3.x, to: npc3.x-100 },
+          fake: { from: 0, to: 0 },
+        },
+        flipY: true,
+        flipX: true,
+        ease: 'Linear',      
+        duration: 1800 - this.level * 400,
+        repeat: -1,       
+        yoyo: true
+      });
+      const npc4 = npcs.create(560, 150, 'npc2')
+        .setScale(0.25)
+        .setAngle(-90)
+        .setOffset(-75, -75)
+        .setCircle(150)
         .setData("mask", true);
-      npcs.create(490, 325, 'npc2').setScale(0.25).setAngle(90).setCircle(60)
+      this.tweens.add({
+        targets: npc4,
+        props: {
+          x: { from: npc4.x, to: npc4.x-120 },
+        },
+        flipY: true,
+        flipX: true,
+        ease: 'Linear',      
+        duration: 1800 - this.level * 400,
+        repeat: -1,       
+        yoyo: true
+      });
+      const npc5 = npcs.create(475, 315, 'npc2')
+        .setScale(0.25)
+        .setAngle(135)
+        .setOffset(-75, -75)
+        .setCircle(150)
         .setData("mask", true);
+      this.tweens.add({
+        targets: npc5,
+        props: {
+          y: { from: npc5.y, to: npc5.y+70 },
+          x: { from: npc5.x, to: npc5.x+130 },
+          fake: {from: 0, to: 0}
+        },
+        flipY: true,
+        flipX: true,
+        ease: 'Linear',      
+        duration: 1800 - this.level * 400,
+        repeat: -1,       
+        yoyo: true
+      });
       this.physics.add.overlap(this.player, npcs, (player, npc) => this.nearNpc(player, npc));
 
       this.handsan = this.physics.add.sprite(35, 580, 'handsan').setScale(0.20);
